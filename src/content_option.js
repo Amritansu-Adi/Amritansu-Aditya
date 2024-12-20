35479396e99290078606c6437358bb14b51079ff
Amritansu-Adi <amritansuaditya1@gmail.com>
2024-12-20 13:30:18 +0530

    putting in repo

---
+++ src/content_option.js
@@ -0,0 +1,85 @@
+import proj1 from './assets/images/proj1.png';
+import proj2 from './assets/images/proj2.png';
+
+
+
+const logotext = "Amritansu";
+const meta = {
+    title: "Amritansu Aditya",
+    description: "I am Amritansu Aditya, Machine Learning and Full stack devloper.",
+};
+
+const introdata = {
+    title: "I'm Amritansu Aditya",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I program machine models.",
+    },
+    description: "I am a Machine Learning and Full stack devloper. I am a passionate programmer and I love to code in C++, Python,  React, and Javascript.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a'bit about my self",
+    aboutme: "I am a Machine Learning and Full stack devloper. I am a quick learner and I love to learn new technologies. I like to work on projects that are challenging and require creative solutions. I am a team player and I enjoy collaborating with others to achieve a common goal. I am passionate about coding and I am always looking for ways to improve my skills and expand my knowledge.",
+};
+
+
+const skills = [{
+        name: "Python 3",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    
+];
+
+
+const dataportfolio = [{
+        img: proj1,
+        description:"Stock Market Tracking website for NASDAQ.",
+        link: "#",
+    },
+    {
+        img: proj2,
+        description: "Disater Relief based web Project.",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "amritansaditya1@gmail.com",
+    YOUR_FONE: "+918340664121",
+    description: "I am available on the following platforms. You can contact me through any of these platforms.",
+    YOUR_SERVICE_ID: "amritansu_adi",
+    YOUR_TEMPLATE_ID: "template_tvucwc9",
+    YOUR_USER_ID: "k7tlclrU2q2BKorNi",
+};
+
+const socialprofils = {
+    github: "https://github.com/Amritansu-Adi/",
+    linkedin: "https://www.linkedin.com/in/amritansu-adi/",
+    instagram: "https://www.instagram.com/lm10amri/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
